b210ead0ec1149375b5288d64e8316d67f161858
Robert Albus <hello@robertalbus.com>
2019-02-03 14:00:58 -0700

    Creating basic App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NavComponent } from './header/nav/nav.component'
+
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'about', redirectTo: '' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
